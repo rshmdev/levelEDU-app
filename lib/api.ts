12dc417533a243adf0bb0e9db578dea875a516e4
Rian Moraes <fortpcnite21@gmail.com>
2025-11-08 21:01:25 -0300

    fix: app

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =  "https://616742b68abb.ngrok-free.app/app";
+const API_BASE_URL =  "https://leveledu-api.onrender.com/app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
